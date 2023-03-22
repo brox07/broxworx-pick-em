972b8719da31cb9f1c3ea7309b3980167eb1d46c
Brandon Broxterman <brandon.broxterman@hanes.com>
2023-03-21 19:57:48 -0500

    fix login/signup

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import { useAuth } from '../AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const LogIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
   const { logIn } = useAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setError('');
       await logIn(email, password);
+      navigate('/pick-em');
     } catch (error) {
       setError('Failed to log in');
     }
@@ -21,6 +25,7 @@ const LogIn = () => {
     <div>
       <h2>Log In</h2>
       {error && <p>{error}</p>}
+      {successMessage && <p>{successMessage}</p>}
       <form onSubmit={handleSubmit}>
         <label>Email</label>
         <input
